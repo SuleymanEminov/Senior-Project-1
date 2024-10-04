f3c4a0b414ad5d546c03377ef0ec92a534afd415
suleymaneminov <suleymaneminov07@gmail.com>
2024-10-04 00:10:38 -0400

    add frontend folder to main repository

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/test/')
+      .then(res => res.json())
+      .then(data => setData(data.data));
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Tennis court booking</h1>
+        <p>Book a court easily in this app</p>
+
+        <p>{data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
